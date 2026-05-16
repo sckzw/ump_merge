e605035f8361f238ecb38f5465751de11019d6fe
shuichi <shuichi.kozawa@gmail.com>
2026-05-16 18:15:18 +0900

    Update file handling

--- ump_merge.ts
+++ ump_merge.ts
@@ -1,5 +1,4 @@
 import * as fs from 'fs';
-import * as path from 'path';
 import { globSync } from 'glob';
 import { CompositeBuffer, UmpReader } from 'googlevideo/ump';
 import { MediaHeader, UMPPartId } from 'googlevideo/protos';
@@ -18,14 +17,9 @@ function handleMedia(part: Part) {
   const headerId = part.data.getUint8(0);
   const dataBuffer = part.data.split(1).remainingBuffer;
   console.log(`Media Part (Associated Header ID: ${headerId}):`, dataBuffer.getLength(), 'bytes');
-  // const totalBuffer = Buffer.concat(dataBuffer.chunks);
-  // console.log(`Write media_file_${counter}: `, totalBuffer.byteLength, 'bytes');
-  // fs.writeFileSync(`media_file_${counter}`, totalBuffer);
-  // counter++;
-
   for (const chunk of dataBuffer.chunks) {
-    console.log('File append size: ', chunk.byteLength, 'bytes');
     fs.appendFileSync(currentVideoId, chunk);
+    console.log(`Added ${chunk.byteLength} bytes of data to ${currentVideoId} file.`);
   }
 }
 
@@ -35,20 +29,18 @@ function handleMediaEnd(part: Part) {
 }
 
 const umpPartHandlers = new Map<UMPPartId, (part: Part) => void>([
-  [ UMPPartId.MEDIA_HEADER, handleMediaHeader ],
-  [ UMPPartId.MEDIA, handleMedia ],
-  [ UMPPartId.MEDIA_END, handleMediaEnd ]
+  [UMPPartId.MEDIA_HEADER, handleMediaHeader],
+  [UMPPartId.MEDIA, handleMedia],
+  [UMPPartId.MEDIA_END, handleMediaEnd]
 ]);
 
-const targetFolder = process.argv[2];
-if (!targetFolder) {
-  console.log('フォルダパスを指定してください。');
+const filePattern = process.argv[2];
+if (!filePattern) {
+  console.log('ファイルパスを指定してください。');
   process.exit(1);
 }
 
-const filePattern = 'videoplayback_*';
-const searchPattern = path.join(targetFolder, filePattern);
-const files = globSync(searchPattern, { nodir: true, windowsPathsNoEscape: true }).sort();
+const files = globSync(filePattern, { nodir: true, windowsPathsNoEscape: true }).sort();
 
 files.forEach((fileName: string, index: number) => {
   console.log(`[${index + 1}/${files.length}] ${fileName} を処理中...`);
